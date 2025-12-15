8e92d682d47ab1112cc03307ccc554d9b3fff297
najmulislamnajim <ninbd17@gmail.com>
2025-12-15 09:50:19 +0600

    add model & migrations file

--- doctors_ai_course2/models.py
+++ doctors_ai_course2/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core.models import Territory
 
 # Create your models here.
+class DoctorAiCourseUp(models.Model):
+    territory = models.ForeignKey(Territory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    rpl_id = models.CharField(max_length=20)
+    specialty = models.CharField(max_length=255)
+    designation = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = 'doctors_ai_course_up'
+        verbose_name = 'Doctors AI Course Up'
+        verbose_name_plural = 'Doctors AI Courses Up'
