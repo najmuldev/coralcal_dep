797f31083856d8c7c9f0522e6c0e54c00f4cf776
Jahidul Islam <jahidul.app@gmail.com>
2026-01-21 17:36:32 +0600

    feat: edit dd choice action

--- doctor_development/urls.py
+++ doctor_development/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from doctor_development.views import book_choice, admin, export_wishlist, edit_choice
+from doctor_development.views import gift_choice, admin, export_wishlist, edit_choice
 
 urlpatterns = [
-    path('gift', book_choice, name='book_choice'),
+    path('gift', gift_choice, name='gift_choice'),
     path('admin',admin, name='admin'),
     path('export_wishlist', export_wishlist, name='export_wishlist'),
-    path('edit_ks/<int:id>', edit_choice, name='edit_ks')
+    path('edit_dd/<int:id>', edit_choice, name='edit_dd')
 ]
--- doctor_development/views.py
+++ doctor_development/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import BookWishes, Territory 
+from .models import DoctorDevelopment, Territory 
 import csv
 from django.http import HttpResponse
 from django.templatetags.static import static
 from django.contrib.auth.decorators import login_required
 
-# map book names
-IMAGE_TO_BOOK = {
-    'image1': '100 diagnostic dilemmas in clinical medicine',
-    'image2': '100 cases in obstetrics and gynaecology',
-    'image3': '100 cases in accute medicine',
+# map gift names
+IMAGE_TO_GIFT = {
+    'image1': 'Araong gift card',
+    'image2': 'Mega Mall gift card',
+    'image3': 'Watch',
 }
 
-def book_choice(request):
+def gift_choice(request):
     if request.method == 'POST':
         dr_id = request.POST.get('dr_id')
         dr_name = request.POST.get('dr_name')
         selected_image = request.POST.get('selected_image')
 
-        # Map image ID to actual book title
-        book_title = IMAGE_TO_BOOK.get(selected_image)
+        # Map image ID to actual gift title
+        gift_title = IMAGE_TO_GIFT.get(selected_image)
 
-        if not (dr_id and dr_name and book_title):
-            messages.error(request, "All fields are required and a book must be selected.")
-            return redirect('book_choice')
+        if not (dr_id and dr_name and gift_title):
+            messages.error(request, "All fields are required and a gift must be selected.")
+            return redirect('gift_choice')
 
         try:
             territory_id = request.user.username
@@ -33,38 +33,38 @@ def book_choice(request):
             print(territory)
             if not territory:
                 messages.error(request, "No territory found.")
-                return redirect('book_choice')
+                return redirect('gift_choice')
 
             # Save data
-            BookWishes.objects.create(
+            DoctorDevelopment.objects.create(
                 territory=territory,
                 dr_id=dr_id,
                 dr_name=dr_name,
-                book=book_title
+                gift=gift_title
             )
-            messages.success(request, f"Successfully submitted book wish for {dr_name}.")
-            return redirect('book_choice')
+            messages.success(request, f"Successfully submitted gift wish for {dr_name}.")
+            return redirect('gift_choice')
         except Exception as e:
             messages.error(request, f"Error occurred: {str(e)}")
-            return redirect('book_choice')
+            return redirect('gift_choice')
     if request.method=='GET':
         territory_id = request.user.username
         try:
-            obj = BookWishes.objects.get(territory__territory=territory_id)
-        except BookWishes.DoesNotExist:
+            obj = DoctorDevelopment.objects.get(territory__territory=territory_id)
+        except DoctorDevelopment.DoesNotExist:
             obj = None
         if obj:
-            book_title = obj.book
-            if book_title == '100 diagnostic dilemmas in clinical medicine':
-                img = static('images/book1.jpg')
-            elif book_title == '100 cases in obstetrics and gynaecology':
-                img = static('images/book2.jpg')
-            elif book_title == '100 cases in accute medicine':
-                img = static('images/book3.jpg')
+            gift_title = obj.gift
+            if gift_title == 'Araong gift card':
+                img = static('images/image001.png')
+            elif gift_title == 'Mega Mall gift card':
+                img = static('images/image002.png')
+            elif gift_title == 'Watch':
+                img = static('images/image003.png')
             else:
-                print("No image found for the book title:", book_title)
+                print("No image found for the gift title:", gift_title)
             return render(request, 'choice.html', {'obj': obj, 'img':img})
-        return render(request, 'book_choice.html')
+        return render(request, 'gift_choice.html')
     
 @login_required
 def edit_choice(request, id):
@@ -72,69 +72,69 @@ def edit_choice(request, id):
         dr_id = request.POST.get('dr_id')
         dr_name = request.POST.get('dr_name')
         selected_image = request.POST.get('selected_image')
-        book_title = IMAGE_TO_BOOK.get(selected_image)
+        gift_title = IMAGE_TO_GIFT.get(selected_image)
 
-        if not (dr_id and dr_name and book_title):
-            messages.error(request, "All fields are required and a book must be selected.")
-            return redirect('edit_choice', id=id)
+        if not (dr_id and dr_name and gift_title):
+            messages.error(request, "All fields are required and a gift must be selected.")
+            return redirect('edit_dd_choice', id=id)
 
         try:
-            obj = BookWishes.objects.get(id=id)
+            obj = DoctorDevelopment.objects.get(id=id)
             obj.dr_id = dr_id
             obj.dr_name = dr_name
-            obj.book = book_title
+            obj.gift = gift_title
             obj.save()
-            messages.success(request, f"Successfully updated book wish for {dr_name}.")
+            messages.success(request, f"Successfully updated gift wish for {dr_name}.")
             if request.user.is_superuser:
-                return redirect('knowledge_series')
-            return redirect('book_choice')
+                return redirect('doctor_development')
+            return redirect('gift_choice')
         except Exception as e:
             messages.error(request, f"Error occurred: {str(e)}")
-            return redirect('edit_choice', id=id)
+            return redirect('edit_dd_choice', id=id)
 
     elif request.method == 'GET':
         try:
-            obj = BookWishes.objects.get(id=id)
-            book_title = obj.book
-            if book_title == '100 diagnostic dilemmas in clinical medicine':
-                img = static('images/book1.jpg')
-            elif book_title == '100 cases in obstetrics and gynaecology':
-                img = static('images/book2.jpg')
-            elif book_title == '100 cases in accute medicine':
-                img = static('images/book3.jpg')
+            obj = DoctorDevelopment.objects.get(id=id)
+            gift_title = obj.gift
+            if gift_title == 'Araong gift card':
+                img = static('images/image001.png')
+            elif gift_title == 'Mega Mall gift card':
+                img = static('images/image002.png')
+            elif gift_title == 'Watch':
+                img = static('images/image003.png')
             else:
                 img = ''
-            return render(request, 'edit_choice.html', {'obj': obj, 'img': img})
-        except BookWishes.DoesNotExist:
+            return render(request, 'edit_dd_choice.html', {'obj': obj, 'img': img})
+        except DoctorDevelopment.DoesNotExist:
             messages.error(request, "Wish not found.")
             if request.user.is_superuser:
-                return redirect('knowledge_series')
-            return redirect('book_choice')
+                return redirect('doctor_development')
+            return redirect('gift_choice')
 
 
 def export_wishlist(request):
     """
-    Export the book wishes to a CSV file.
+    Export the gift wishes to a CSV file.
     """
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="book_wishes.csv"'
+    response['Content-Disposition'] = 'attachment; filename="gift_wishes.csv"'
 
     writer = csv.writer(response)
-    writer.writerow(['Territory', 'Territory Name', 'Region Name', 'Zone Name', 'Doctor ID', 'Doctor Name', 'Book'])
+    writer.writerow(['Territory', 'Territory Name', 'Region Name', 'Zone Name', 'Doctor ID', 'Doctor Name', 'Gift'])
 
-    wishes = BookWishes.objects.all()
+    wishes = DoctorDevelopment.objects.all()
     for wish in wishes:
-        writer.writerow([wish.territory.territory, wish.territory.territory_name, wish.territory.region_name,wish.territory.zone_name, wish.dr_id, wish.dr_name, wish.book])
+        writer.writerow([wish.territory.territory, wish.territory.territory_name, wish.territory.region_name,wish.territory.zone_name, wish.dr_id, wish.dr_name, wish.gift])
 
     return response
 
 def admin(request):
     """
-    Render the admin page with book wishes.
+    Render the admin page with gift wishes.
     """
     if not request.user.is_superuser:
         messages.error(request, "You do not have permission to access this page.")
         return redirect('login')
 
-    wishes = BookWishes.objects.all()
+    wishes = DoctorDevelopment.objects.all()
     return render(request, 'admin.html', {'wishes': wishes})
