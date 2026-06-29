37ecbe4b6870477169757b8874a2066e3c7added
najmulislamnajim <ninbd17@gmail.com>
2026-06-29 14:54:46 +0600

    add model

--- nfc_card_info/models.py
+++ nfc_card_info/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class NfcCardInfo(models.Model):
+    dr_rpl_id = models.CharField(max_length=10, unique=True)
+    dr_name = models.CharField(max_length=155)
+    degree = models.CharField(max_length=255)
+    specialty = models.CharField(max_length=255)
+    institute = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add = True)
+    
+    class Meta:
+        db_table = "nfc_card_info"
+        verbose_name = "NFC Card Info"
+        verbose_name_plural = "NFC Card Infos"
